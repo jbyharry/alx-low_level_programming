dccc036fc2019793a1fb8b9e9d33727deca7ff63
jbyharry <refugekid@gmail.com>
2022-09-08 18:20:09 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
-#include <stdio.h>                                                                                                                            
-                                                                                                                                              
-/**                                                                                                                                           
- *  * main - Entry point                                                                                                                         
- *   *                                                                                                                                            
- *    * Return: always 0                                                                                                                           
- *     */                                                                                                                                           
-int main(void)                                                                                                                                
-{                                                                                                                                             
-	        puts("\"Programming is like building a multilingual puzzle");                                                                         
-		        /**                                                                                                                                   
-			 *          * return value - return value for main                                                                                               
-			 *                   *                                                                                                                                    
-			 *                            * Return: always 0                                                                                                                   
-			 *                                     */                                                                                                                                   
-		        return (0);                                                                                                                           
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
