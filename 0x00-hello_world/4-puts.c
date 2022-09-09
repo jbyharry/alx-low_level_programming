f6781f317ca7e2e2c73362aac773d6914d379b6a
jbyharry <refugekid@gmail.com>
2022-09-08 18:10:40 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,17 @@
-#include <stdio.h>
-
-
-
-/**
- * main - Entry point
- *
- * Return: Always 0 
- */
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-		/**
-		 * return value - return value for main
-		 *
-		 * Return: always 0
-		 */
-
+#include <stdio.h>                                                                                                                            
+                                                                                                                                              
+/**                                                                                                                                           
+ *  * main - Entry point                                                                                                                         
+ *   *                                                                                                                                            
+ *    * Return: always 0                                                                                                                           
+ *     */                                                                                                                                           
+int main(void)                                                                                                                                
+{                                                                                                                                             
+	        puts("\"Programming is like building a multilingual puzzle");                                                                         
+		        /**                                                                                                                                   
+			 *          * return value - return value for main                                                                                               
+			 *                   *                                                                                                                                    
+			 *                            * Return: always 0                                                                                                                   
+			 *                                     */                                                                                                                                   
+		        return (0);                                                                                                                           
 }
