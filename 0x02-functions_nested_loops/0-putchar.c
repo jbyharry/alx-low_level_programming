be918a45d33367040d9872c93e3d66f60bc1cda1
jbyharry <refugekid@gmail.com>
2022-09-13 01:19:33 -0700

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('');
+	_putchar('');
+	_putchar('');
+	_putchar('');
+	_putchar('');
+	_putchar('\n');
+	return (0);
+}
